9cc649e081ae327976cdee60fa5406f097326beb
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-03-06 12:16:56 +0100

    01 modern redux toolkit: basic example counter

---
+++ 01-learn-modern-redux-toolkit/src/app/store.ts
@@ -0,0 +1,16 @@
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "./features/counter/counterSlice";
+
+const store = configureStore({
+  reducer: {
+    counter: counterReducer,
+  },
+});
+
+// infer the 'RootState' and 'AppDispatch' types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+
+// inferred type
+export type AppDispatch = typeof store.dispatch;
+
+export default store;
